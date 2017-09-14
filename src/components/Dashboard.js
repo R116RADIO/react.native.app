327eec65e6b893b8a521d5c21ae41d73ef518364
mobile.soft127@gmail.com <mobile.soft127@gmail.com>
2017-09-14 15:54:22 +0300

    complete-ui

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,7 +13,7 @@ import {
 import  Constant  from '../common/Constant'
 
 var {height, width} = Dimensions.get('window');
-
+var isPlay = false
 
 // create a component
 class Dashboard extends Component {
@@ -22,9 +22,10 @@ class Dashboard extends Component {
         this.state = {
             pageHeight:height,
             pageWidth:width,
-            isSlow: false,
+            isSlow: true,
             isHip: false,
-            isPop: true,
+            isPop: false,
+            isPlay: false,
         };
     }
     componentWillMount() {
@@ -36,26 +37,83 @@ class Dashboard extends Component {
             pageWidth:event.nativeEvent.layout.width,
         });
     }
-    render() {
-        
+    onShow = () => {
+        this.setState({
+            isSlow: true,
+            isHip: false,
+            isPop: false
+        })
+    }
+    onHip = () => {
+        this.setState({
+            isSlow: false,
+            isHip: true,
+            isPop: false
+        })
+    }
+    onPop = () => {
+        this.setState({
+            isSlow: false,
+            isHip: false,
+            isPop: true
+        })
+    }
+    onPressPlay = () => {
+        isPlay =! isPlay
+        this.setState({ isPlay: isPlay })
+    }
+    showPlayButton(){
+        return(
+            <TouchableWithoutFeedback onPress = {this.onPressPlay}>
+                <Image source = {this.state.isPlay? require('../assets/play.png'):require('../assets/stop.png')} style = {styles.startImg}></Image>
+            </TouchableWithoutFeedback>
+        )
+    }
+    showSongInfo(){
+        if(this.state.pageWidth < 600){
+            return(
+                <View style = {{alignItems:'center', marginTop: 50}}>
+                    <Image source = {require('../assets/music.png')} style = {styles.musicImg}/>
+                    <View style = {{flexDirection:'row', marginTop: 30, alignItems:'center'}}>
+                        {this.showPlayButton()}
+                        <View style = {{marginLeft: 20}}>
+                            <Text style = {styles.songtitle}>10 Hands</Text>
+                            <Text style = {styles.artist}>Jor'dan Armstrong</Text>
+                            <Text style = {styles.playlisttitle}>RnB Soul</Text>
+                        </View>
+                    </View>
+                </View>
+            )
+        }
+        else{
+            return(
+                <View style = {{flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+                    <Image source = {require('../assets/music.png')} style = {[styles.musicImg]}/>
+                    <View style = {{flexDirection:'row', marginLeft: 20}}>
+                        {this.showPlayButton()}
+                        <View style = {{marginLeft: 20, justifyContent:'center'}}>
+                            <Text style = {styles.songtitle}>10 Hands</Text>
+                            <Text style = {styles.artist}>Jor'dan Armstrong</Text>
+                            <Text style = {styles.playlisttitle}>RnB Soul</Text>
+                        </View>
+                    </View>
+                </View>
+            )
+        }
+    }
+    render() {        
         return (
             <View style={styles.container} onLayout={this._onLayout}>
                 <View style = {styles.mainView}>
                     <Image source = {require('../assets/logo-radio.jpg')} style = {styles.logoImg}/>
-                    <Image source = {require('../assets/music.png')} style = {styles.musicImg}/>
-                    <View style = {{flexDirection:'row', marginTop: 30}}>
-                        <Image source = {require('../assets/play.png')} style = {styles.startImg}></Image>
-                        <View style = {{marginLeft: 20}}>
-                            <Text style = {{color:'white', fontSize: 15}}>10 Hands</Text>
-                            <Text style = {{color:'white', fontSize: 13}}>Jor'dan Armstrong</Text>
-                            <Text style = {{color:'gray', fontSize: 11}}>RnB Soul</Text>
-                        </View>
-                    </View>
+                    {this.showSongInfo()}
+                    <Text style = {[styles.genre, {width: this.state.pageWidth}]}>Choose your genre:</Text>
+                    
                 </View>
 
                 <View style = {[styles.tabView, { width: this.state.pageWidth }]}>
-                    <TouchableWithoutFeedback>
-                        <View style = {[styles.tab1, {width: this.state.pageWidth/3}]}>
+                    <TouchableWithoutFeedback onPress = {this.onShow}>
+                        <View style = {[styles.tab1, {width: this.state.pageWidth/3}, this.state.isSlow?{backgroundColor: '#460607'}:{backgroundColor: 'transparent'}]}>
                             <Text style = {styles.tab1_name}>Slow Jams</Text>
                             {this.state.isSlow?
                                 <View style = { [styles.line, {width: this.state.pageWidth/3}]}/> :
@@ -64,8 +122,8 @@ class Dashboard extends Component {
                         </View>
                     </TouchableWithoutFeedback>
 
-                    <TouchableWithoutFeedback>
-                        <View style = {[styles.tab1, {width: this.state.pageWidth/3}]}>
+                    <TouchableWithoutFeedback onPress = {this.onHip}>
+                        <View style = {[styles.tab1, {width: this.state.pageWidth/3},this.state.isHip?{backgroundColor: '#460607'}:{backgroundColor: 'transparent'}]}>
                             <Text style = {styles.tab1_name}>Hip Hop</Text>
                             {this.state.isHip?
                                 <View style = { [styles.line, {width: this.state.pageWidth/3}]}/> :
@@ -74,8 +132,8 @@ class Dashboard extends Component {
                         </View>
                     </TouchableWithoutFeedback>
 
-                    <TouchableWithoutFeedback>
-                        <View style = {[styles.tab1, {width: this.state.pageWidth/3}]}>
+                    <TouchableWithoutFeedback onPress = {this.onPop}>
+                        <View style = {[styles.tab1, {width: this.state.pageWidth/3},this.state.isPop?{backgroundColor: '#460607'}:{backgroundColor: 'transparent'}]}>
                             <Text style = {styles.tab1_name}>Pop Music</Text>
                             {this.state.isPop?
                                 <View style = { [styles.line, {width: this.state.pageWidth/3}]}/> :
@@ -97,7 +155,9 @@ const styles = StyleSheet.create({
         backgroundColor: 'black',
     },
     mainView: {
-        alignItems: 'center'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
     },
     tabView: {
         flexDirection: 'row',
@@ -108,17 +168,16 @@ const styles = StyleSheet.create({
         left: 0,
     },
     tab1: {
-        // backgroundColor: '#460607',
         alignItems:'center',
         justifyContent: 'center',
         height: 50,
     },
     tab1_name: {
         color: 'white',
-        fontSize: 18
+        fontSize: 17
     },
     line: {
-        height: 6,
+        height: 5,
         backgroundColor: 'white',
         position: 'absolute',
         bottom: 0,
@@ -126,7 +185,7 @@ const styles = StyleSheet.create({
     },
     logoImg: {
         width: width/4,
-        height: 200,
+        height: 100,
         resizeMode: 'contain'
     },
     musicImg: {
@@ -135,8 +194,30 @@ const styles = StyleSheet.create({
         resizeMode: 'cover'
     },
     startImg: {
-        width: width/8,
-        height: width/8,
+        width: width/6,
+        height: width/6,
+    },
+    songtitle: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    artist: {
+        color: 'white',
+        fontSize: 14,
+        marginTop: 5,
+    },
+    playlisttitle: {
+        color: 'gray',
+        fontSize: 12,
+        marginTop: 5,
+    },
+    genre:{
+        color: 'white',
+        fontSize: 17,
+        marginBottom: 60,
+        textAlign: 'left',
+        paddingLeft: 15
     }
 });
 
